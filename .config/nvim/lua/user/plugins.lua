1b7287c575f95433437c7d3b168c743cebd4b29a
DrDrei <9737270+DrDrei@users.noreply.github.com>
2022-09-21 17:47:51 -0600

    feat(nvim): added treesitter

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -46,6 +46,10 @@ return packer.startup(function(use)
     "nvim-telescope/telescope.nvim",
     requires = { {'nvim-lua/plenary.nvim'} },
   })
+  use({
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  })
 
   use("wbthomason/packer.nvim")
   if PACKER_BOOTSTRAP then
