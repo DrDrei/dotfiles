35b9ad999270648efa3129e3fd24876cbbe409eb
DrDrei <9737270+DrDrei@users.noreply.github.com>
2022-09-29 01:31:22 -0600

    feat(hotkeys): quiet pushes

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -4,36 +4,36 @@ if not status_ok then
 end
 
 local on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true }
+  local opts = { noremap = true, silent = true }
 
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local opts = {
-	capabilities = capabilities,
-	on_attach = on_attach
+  capabilities = capabilities,
+  on_attach = on_attach
 }
 
 lspconfig.tsserver.setup(opts)
 
 lspconfig.sumneko_lua.setup {
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' }
-			}
-		}
-	}
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
 }
