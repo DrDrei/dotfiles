d2062bcfce92340090aba2ab86641332486fca8c
DrDrei <9737270+DrDrei@users.noreply.github.com>
2022-10-03 14:55:25 -0600

    feat(configs): updated lsp, hotkeys and iterm2

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -1,50 +1,57 @@
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
 local lsp_defaults = {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-  ),
-  on_attach = function()
-    vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
-  end
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	on_attach = function()
+		vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+	end,
 }
 
-lspconfig.util.default_config = vim.tbl_deep_extend(
-  'force',
-  lspconfig.util.default_config,
-  lsp_defaults
-)
+lspconfig.util.default_config = vim.tbl_deep_extend("force", lspconfig.util.default_config, lsp_defaults)
 
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'LspAttached',
-  desc = 'LSP actions',
-  callback = function(_, bufnr)
-    local opts = { noremap = true, silent = true }
+vim.api.nvim_create_autocmd("User", {
+	pattern = "LspAttached",
+	desc = "LSP actions",
+	callback = function(_, bufnr)
+		local opts = { noremap = true, silent = true }
 
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-  end
+		local bufopts = { noremap = true, silent = true, buffer = bufnr }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+		vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bufopts)
+		vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	end,
 })
 
-lspconfig.tsserver.setup({ })
+vim.diagnostic.config({
+	virtual_text = false,
+})
+
+lspconfig.tsserver.setup({
+	on_attach = function(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+		lspconfig.util.default_config.on_attach(client, bufnr)
+	end,
+})
+
+lspconfig.tailwindcss.setup({})
 
 lspconfig.sumneko_lua.setup({
-  single_file_support = true,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
+	single_file_support = true,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
