896a327034118daf92e3f62ea21e7ee80eb7cebf
DrDrei <9737270+DrDrei@users.noreply.github.com>
2022-10-03 22:52:00 -0600

    feat(nvim-tree): close tree on file open

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,36 +1,41 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
 nvim_tree.setup({
-  respect_buf_cwd = true,
-  update_focused_file = {
-    enable = false,
-    update_cwd = true,
-  },
-  renderer = {
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      },
-    },
-  },
+	respect_buf_cwd = true,
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+	update_focused_file = {
+		enable = false,
+		update_cwd = true,
+	},
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
 })
