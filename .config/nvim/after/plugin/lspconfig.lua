12469376c238afb988f6bb0a5c2b40261732a6e5
DrDrei <9737270+DrDrei@users.noreply.github.com>
2022-10-19 14:35:01 -0600

    feat(highlighting): highlight references

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -30,8 +30,24 @@ local clients = {
 
 lspconfig.util.default_config = vim.tbl_deep_extend("force", lspconfig.util.default_config, {
 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-	on_attach = function()
+	on_attach = function(client, bufnr)
 		vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+		if client.server_capabilities.documentHighlightProvider then
+			vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = vim.lsp.buf.document_highlight,
+				buffer = bufnr,
+				group = "lsp_document_highlight",
+				desc = "Document Highlight",
+			})
+			vim.api.nvim_create_autocmd("CursorMoved", {
+				callback = vim.lsp.buf.clear_references,
+				buffer = bufnr,
+				group = "lsp_document_highlight",
+				desc = "Clear All the References",
+			})
+		end
 	end,
 })
 
